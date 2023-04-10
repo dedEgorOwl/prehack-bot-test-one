16483d9cfd23c1153803484e5ffc4c62752c20a5
dedEgor <egorowllebedev@gmail.com>
2023-04-10 21:36:22 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import { useEffect } from 'react';
 import { TextField } from '@mui/material';
 import './App.scss'
 
-const tg = window.Telegram.WebApp;
-const mainColor = tg.themeParams.button_color;
+
 
 function App() {
+	const tg = window.Telegram.WebApp;
+	const mainColor = tg.themeParams.button_color;
 
 	tg.MainButton.text = "Заказать";
 	tg.MainButton.show();
@@ -18,8 +19,7 @@ function App() {
 
   return (
 	<div className="App">
-		{mainColor}
-		<TextField id="outlined-search" label="Company name" type="search" />
+		<TextField id="outlined-search" color={mainColor} label="Company name" type="search" />
 	</div>
   )
 }
