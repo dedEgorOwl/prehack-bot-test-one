75271a3d0ca7d112093952d2c47d2d62f004a483
dedEgor <egorowllebedev@gmail.com>
2023-04-10 20:24:37 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Input, ConfigProvider } from 'antd';
 import './App.scss'
 
 const tg = window.Telegram.WebApp;
+const themeParams = tg.themeParams;
 
 function App() {
 
@@ -16,7 +17,21 @@ function App() {
 	
 
   return (
-	<ConfigProvider theme={{token: {colorPrimary: tg.MainButton.color, lineWidthBold: 4, lineWidthFocus: 8}}}>
+	<ConfigProvider theme={
+		themeParams.text_color
+		  ? {
+			  algorithm:
+				colorScheme === 'dark'
+				  ? theme.darkAlgorithm
+				  : theme.defaultAlgorithm,
+			  token: {
+				colorText: themeParams.text_color,
+				colorPrimary: themeParams.button_color,
+				colorBgBase: themeParams.bg_color
+			  },
+			}
+		  : undefined
+	  }>
 		<div className="App">
 			<Input size='large' placeholder='Company name' className='inpTest'/>
 	
