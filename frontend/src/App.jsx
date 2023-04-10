b94da87b29aa3db05ea8ba610b604046b35d9e34
dedEgor <egorowllebedev@gmail.com>
2023-04-10 21:34:45 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { TextField } from '@mui/material';
 import './App.scss'
 
 const tg = window.Telegram.WebApp;
-const mainColor = tg.themeParams;
+const mainColor = tg.themeParams.button_color;
 
 function App() {
 
@@ -19,7 +19,7 @@ function App() {
   return (
 	<div className="App">
 		{mainColor}
-		<TextField id="outlined-search" color={mainColor} label="Company name" type="search" />
+		<TextField id="outlined-search" label="Company name" type="search" />
 	</div>
   )
 }
